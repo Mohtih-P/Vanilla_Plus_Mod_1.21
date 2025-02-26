1eef348cc5ba51e54e8729795f0bf49cd36d13d7
The-BigLong-Community <biglongcommunity@gmail.com>
2025-02-27 01:58:39 +0530

    Initial commit

---
+++ src/main/java/net/mohithp/vanillaplusmod/VanillaModClient.java
@@ -0,0 +1,10 @@
+package net.mohithp.vanillaplusmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mohithp/vanillaplusmod/VanillaModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mohithp.vanillaplusmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanillaModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
